70dcd42950568dc5874c048ccc29f0b7b06cc9b4
Aditya Mahaddalkar <adityam1311@gmail.com>
2020-09-23 15:48:24 +0530

    JUnit tester defined with mocking of Calculator interface

---
+++ src/test/java/service/ApplicationTester.java
@@ -0,0 +1,33 @@
+package service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ApplicationTester {
+    @Mock
+    CalculatorService calculatorService;
+
+    @InjectMocks
+    Application application = new Application(calculatorService);
+
+    @Test
+    public void testingAdd(){
+        when(calculatorService.add(10, 20)).thenReturn(30);
+        int expected = 30;
+        Assert.assertEquals(expected, application.add(10, 20));
+    }
+
+    @Test
+    public void testingSubtract(){
+        when(calculatorService.subtract(10, 20)).thenReturn(-10);
+        int expected = -10;
+        Assert.assertEquals(expected, application.subtract(10, 20));
+    }
+}
